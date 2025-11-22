8a23985a5864af368f49248afc55017361421194
Triveni_V <97962294+VadduriTriveni@users.noreply.github.com>
2025-11-22 19:35:23 +0530

    Update PlaylistController.java

--- src/controller/PlaylistController.java
+++ src/controller/PlaylistController.java
@@ -1,11 +1,7 @@
-package io.educative.mediaapp.controller;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import io.educative.mediaapp.service.PlaylistService;
 
 @Controller
 public class PlaylistController {
