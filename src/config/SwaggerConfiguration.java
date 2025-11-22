0345ca4b7cf424c4158b7a70c2f36d9cb13a7ccd
Triveni_V <97962294+VadduriTriveni@users.noreply.github.com>
2025-11-22 19:33:38 +0530

    Update SwaggerConfiguration.java

--- src/config/SwaggerConfiguration.java
+++ src/config/SwaggerConfiguration.java
@@ -1,5 +1,3 @@
-package io.educative.mediaapp.config;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
